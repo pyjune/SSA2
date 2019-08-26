cf94530c68c60da27acc935a6c90703232107de4
pyjune <pyjune@naver.com>
2019-08-26 09:48:55 +0900

    Update 달팽이2.py

--- 0826/달팽이2.py
+++ 0826/달팽이2.py
@@ -17,7 +17,7 @@ for tc in range(1, T+1):
     i = 0 # 현재칸 좌표
     j = 0
     k = 1 # 칸에 기록할 값
-    while(k <= 9): # 아직 NxN칸 이내면
+    while(k <= N*N): # 아직 NxN칸 이내면
         arr[i][j] = k # 현재칸에 값을 쓰고
         k += 1 # 다음칸에 쓸 값 결정
         # 다음칸을 결정. 배열을 벗어나지 않고 비어있어야 함
@@ -30,5 +30,11 @@ for tc in range(1, T+1):
             dir = (dir+1)%4
             i += di[dir]
             j += dj[dir]
-    print(arr)
+    print('#{}'.format(tc))
+    for i in range(N):
+        for j in range(N):
+            print(arr[i][j], end=' ')
+        print()
+
+
 
